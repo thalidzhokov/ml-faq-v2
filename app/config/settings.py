f26c28e97dd4e60570ffd8c67446e37fba058b03
Albert Thalidzhokov <thalidzhokov@gmail.com>
2018-10-04 15:48:51 +0300

    Upd. app

--- app/config/settings.py
+++ app/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
