f789f688eae46ec82372e1276b397ae4662613b3
Albert Thalidzhokov <thalidzhokov@gmail.com>
2018-10-07 23:12:35 +0300

    Upd. env

---
+++ app/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! -f entrypoint.lock ]; then
+
+    python manage.py collectstatic
+    python manage.py migrate
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('$APP_USER', '$APP_USER_EMAIL', '$APP_USER_PASSWORD')" | python manage.py shell
+
+    touch entrypoint.lock
+
+fi
+
+uwsgi --ini uwsgi.ini
