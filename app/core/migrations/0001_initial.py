7a67d8d6bc29ad585717ace1a4ae4b5737b77849
Albert Thalidzhokov <thalidzhokov@gmail.com>
2018-10-11 20:48:27 +0300

    Upd. migration

--- app/core/migrations/0001_initial.py
+++ app/core/migrations/0001_initial.py
@@ -1,4 +1,5 @@
 from django.db import migrations, models
+import uuid
 
 
 class FAQ(migrations.Migration):
@@ -11,14 +12,14 @@ class FAQ(migrations.Migration):
         migrations.CreateModel(
             name='Answer',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Идентификатор ответа')),
                 ('answer', models.TextField(blank=True, help_text='Ответ', null=True)),
             ],
         ),
         migrations.CreateModel(
             name='Question',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Идентификатор вопроса')),
                 ('question', models.TextField(blank=True, help_text='Вопрос', null=True)),
                 #('answer_id', models.ForeignKey()), <<<<<<<<<<<<<<<<<<
             ],
