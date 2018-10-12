dfe4b1d540e557a1a9f761ff988c6d2725d28e1c
Albert Thalidzhokov <thalidzhokov@gmail.com>
2018-10-12 18:22:03 +0300

    Del. migrations and upd. volume config

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,6 +3,7 @@
 if [ ! -f "entrypoint.lock" ]; then
 
     python manage.py collectstatic
+    python manage.py makemigrations
     python manage.py migrate
     echo "from django.contrib.auth.models import User; User.objects.create_superuser('$APP_USER', '$APP_USER_EMAIL', '$APP_USER_PASSWORD')" | python manage.py shell
 
