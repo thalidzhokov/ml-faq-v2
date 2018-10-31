d80adaf9ec49aa2d988d449ee649d2cfb100bf4f
Albert Thalidzhokov <thalidzhokov@gmail.com>
2018-11-01 00:37:34 +0300

    Upd. env

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ ! -f "entrypoint.lock" ]; then
+if [ ! -f "/tmp/entrypoint.lock" ]; then
 
     python manage.py collectstatic
     python manage.py makemigrations faq
     python manage.py migrate
     echo "from django.contrib.auth.models import User; User.objects.create_superuser('$APP_USER', '$APP_USER_EMAIL', '$APP_USER_PASSWORD')" | python manage.py shell
 
-    touch entrypoint.lock
+    date >> /tmp/entrypoint.lock
 
 fi
 
